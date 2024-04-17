f6f3072362bdb29a30f5175592a57e1a8edea95b
sbernal <soraimar.bernal@gmail.com>
2024-04-17 00:38:29 -0400

    first commit

---
+++ src/main/java/com/example/RickAndMortyService/RickAndMortyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.RickAndMortyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickAndMortyServiceApplication.class, args);
+	}
+
+}
